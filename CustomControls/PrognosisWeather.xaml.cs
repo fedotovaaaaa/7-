ee281a006e2f4fbc45e40ab457428657548b5a6d
fedotovaaaaa <156669756+fedotovaaaaa@users.noreply.github.com>
2024-09-30 14:51:24 +0300

    Initial commit

---
+++ CustomControls/PrognosisWeather.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.DataModel;
+
+namespace WpfApp2.CustomControls
+{
+    /// <summary>
+    /// Логика взаимодействия для PrognosisWeather.xaml
+    /// </summary>
+    public partial class PrognosisWeather : UserControl
+    {
+        private Prognosis _thisprognosis;
+        public Prognosis prognosis
+        {
+            get { return _thisprognosis; }
+            set
+            {
+                _thisprognosis = value
+                    ?? throw new ArgumentNullException(nameof(_thisprognosis));
+            }
+        }
+        public PrognosisWeather(Prognosis progn)
+        {
+            InitializeComponent();
+            prognosis = progn;
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonShowWeather_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void PrognosisWeatherControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Windows/ShowWeatherWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using WpfApp2.Classes;
+using WpfApp2.CustomControls;
+using WpfApp2.DataModel;
+
+namespace WpfApp2.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowWeatherWindow.xaml
+    /// </summary>
+    public partial class ShowWeatherWindow : Window
+    {
+        private Prognosis prognosis;
+        public ShowWeatherWindow(Prognosis prog)
+        {
+            InitializeComponent();
+            prognosis = prog;
+            labelContent.Content += " " + prognosis.Date.Value.ToShortTimeString();
+            LoadDataWrapPanel(prog.Time);
+        }
+
+        private void LoadDataWrapPanel(string info)
+        {
+            List<ComplexWeatherInfo> infos;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ComplexWeatherInfo>));
+            using (StringReader reader = new StringReader(info)) 
+            {
+                infos = xmlSerializer.Deserialize(reader) as List<ComplexWeatherInfo>;
+            }
+            infos.ForEach(item => wrapPanelWeather.Children.Add(new WeatherCustomControl(item)));
+        }
+    }
+}
